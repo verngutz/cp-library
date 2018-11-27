e78355da738c2da28956ae4599519584f3bad8bd
Vernon Gutierrez <vern_go_123@yahoo.com>
2018-11-27 10:27:26 +0800

    Fix DAG longest path.

--- Graph - DAG Longest Path (unweighted).cpp
+++ Graph - DAG Longest Path (unweighted).cpp
@@ -2,17 +2,17 @@
 #include "Graph - Transpose Graph (unweighted).cpp"
 using namespace std;
 using ll = long long;
-ll longest_path(graph<1>& g) {
+ll longest_path(graph<1>& g, const vector<int>& node_weights = vector<int>()) {
     graph<1> t = transpose(g);
     vector<ll> memo(g.n + 1, -1);
     vector<int> pred(g.n + 1, -1);
     function<ll(int)> longest_path = [&](int v) {
         ll& ans = memo[v];
         if(ans == -1) {
-            ans = 0;
+            ans = node_weights.empty() ? 0 : node_weights[v];
             for(auto [i, v, u] : t.adj[v]) {
-                if(ans < longest_path(u) + 1) {
-                    ans = longest_path(u) + 1;
+                if(ans < longest_path(u) + (node_weights.empty() ? 0 : node_weights[v])) {
+                    ans = longest_path(u) + (node_weights.empty() ? 0 : node_weights[v]);
                     pred[v] = u;
                 }
             }
