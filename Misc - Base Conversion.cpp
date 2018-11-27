4c7984a0c1adc63d5b95c454eb5d3aa0bb3f64c9
Vernon Gutierrez <vern_go_123@yahoo.com>
2018-11-27 13:26:39 +0800

    Rename base conversion.

---
+++ Misc - Base Conversion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+string int_to_string(T n, int radix = 10) {
+    if(n == 0) {
+        return "0";
+    } else {
+        string ans;
+        while(n > 0) {
+            ans += n % radix <= 9 ? n % radix + '0' : n % radix - 10 + 'A';
+            n /= radix;
+        }
+        while(ans.back() == '0') {
+            ans.pop_back();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+}
+template <typename T>
+T string_to_int(string& s, int radix = 10) {
+    T ans = 0, pow = 1;
+    for(int i = s.length() - 1; i >= 0; i--) {
+        ans += ('0' <= s[i] and s[i] <= '9' ? s[i] - '0' : 10 + s[i] - 'A') * pow;
+        pow *= radix;
+    }
+    return ans;
+}
