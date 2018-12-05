56e7c3144871ca4f9070acbbf03cc5460861dadb
Vernon Gutierrez <vern_go_123@yahoo.com>
2018-12-06 00:54:35 +0800

    Fix trie.

--- Data Structs - Trie (binary).cpp
+++ Data Structs - Trie (binary).cpp
@@ -1,58 +1,40 @@
 #include <bits/stdc++.h>
 #include "Data Structs - Trie.cpp"
 using namespace std;
-template <typename T>
-class binary_trie {
-private:
-    static const int BITS = numeric_limits<T>::digits;
-    using bitset_t = bitset<BITS>;
-    using trie_t = trie<bitset_t, BITS - 1, -1>;
-    trie_t* t;
-    T min_xor(trie_t* t, const bitset_t& s, int i = BITS - 1) {
-        if(i >= 0) {
-            if(t->has(s[i])) {
-                return min_xor(t->get(s[i]), s, i - 1);
-            } else {
-                return (T(1) << i) | min_xor(t->get(~s[i]), s, i - 1);
-            }
+template <typename T> constexpr int n_bits() { return numeric_limits<T>::digits; }
+template <typename T> using binary_trie_base = trie<2, 0, bitset<n_bits<T>()>, n_bits<T>() - 1, -1>;
+template <typename T> struct binary_trie : public binary_trie_base<T> {
+    using bitset_t = bitset<n_bits<T>()>;
+    void insert(T x) { binary_trie_base<T>::insert(bitset_t(x)); }
+    void erase(T x) { binary_trie_base<T>::erase(bitset_t(x)); }
+    int count(T x) { return binary_trie_base<T>::count(bitset_t(x)); }
+    T min_xor(const bitset_t& s) {
+        if(this->size == 0) {
+            return T(s.to_ullong());
         } else {
-            return 0;
+            binary_trie_base<T>* t = this;
+            T ans = 0;
+            for(int i = n_bits<T>() - 1; i >= 0; i--) {
+                ans += t->get(s[i]) ? 0 : T(1) << i;
+                t = t->get(s[i]) ? t->get(s[i]) : t->get(!s[i]);
+            }
+            return ans;
         }
     }
-    T mex(trie_t* t, const bitset_t& s, int i = BITS - 1) {
-        if(i >= 0 and t and t->has(s[i])) {
-            if(t->get(s[i])->size() < (T(1) << i)) {
-                return mex(t->get(s[i]), s, i - 1);
+    T min_xor(T x = 0) { return min_xor(bitset_t(x)); }
+    T max_xor(T x = 0) { return ~min_xor(~x); }
+    T mex(const bitset_t& s, int i = n_bits<T>() - 1) {
+        binary_trie_base<T>* t = this;
+        T ans = 0;
+        for(int i = n_bits<T>() - 1; i >= 0 and t and t->get(s[i]); i--) {
+            if(t->get(s[i])->size < T(1) << i) {
+                t = t->get(s[i]);
             } else {
-                return (T(1) << i) | mex(t->get(~s[i]), s, i - 1);
+                t = t->get(!s[i]);
+                ans += T(1) << i;
             }
-        } else {
-            return 0;
         }
+        return ans;
     }
-public:
-    binary_trie() : t(new trie_t()) {}
-    int size() {
-        return t->size();
-    }
-    binary_trie& insert(T x) {
-        t->insert(bitset_t(x));
-        return *this;
-    }
-    binary_trie& erase(T x) {
-        t->erase(bitset_t(x));
-        return *this;
-    }
-    int count(T x) {
-        return t->count(bitset_t(x));
-    }
-    T min_xor(T x) {
-        return min_xor(t, bitset_t(x));
-    }
-    T max_xor(T x) {
-        return ~min_xor(~x);
-    }
-    T mex(T x = 0) {
-        return mex(t, bitset_t(x));
-    }
+    T mex(T x = 0) { return mex(bitset_t(x)); }
 };
--- Data Structs - Trie.cpp
+++ Data Structs - Trie.cpp
@@ -1,130 +1,82 @@
 #include <bits/stdc++.h>
-#include "callable.hpp/callable.hpp"
 using namespace std;
-template <typename string_t = string, int string_start = 0, int string_move = 1>
-class trie {
-private:
-    int sz, n_end;
-    map<char, trie*> children;
-    bool depth_valid;
-    int depth;
-    bool in_range(const string_t& s, int i) {
-        return string_move > 0 ? i < s.size() : i >= 0;
-    }
-    trie* node(const string_t& s, int i = string_start) {
-        if(in_range(s, i)) {
-            return has(s[i]) ? get(s[i])->node(s, i + string_move) : nullptr;
-        } else {
-            return this;
+template <int alpha_size, char alpha_start, typename string_t = string, int i_start = 0, int i_step = 1>
+struct trie {
+    int size, n_ending, height;
+    vector<trie*> children;
+    trie() : size(0), n_ending(0), height(0), children(alpha_size) {}
+    ~trie() {
+        for(trie* t : children) {
+            delete t;
         }
+    }
+    trie*& get(char c) {
+        return children[c - alpha_start];
+    }
+    trie*& operator[](char c) {
+        return get(c);
+    }
+    trie* operator[](const string_t& s) {
+        auto t = this;
+        for(int i = i_start; 0 <= i and i < s.size(); i += i_step) {
+            t = t ? t->get(s[i]) : nullptr;
+        }
+        return t;
     }
     void remove(char c) {
         delete children[c];
-        children.erase(c);
+        children[c] = nullptr;
     }
-    void compute_depth() {
-        depth = 0;
-        for(auto& c : children) {
-            depth = max(depth, c.second->get_depth() + 1);
-        }
-        depth_valid = true;
-    }
-public:
-    trie() : sz(0), n_end(0), depth_valid(false), depth(0) {}
-    int size() {
-        return sz;
-    }
-    int n_ending() {
-        return n_end;
-    }
-    int get_depth() {
-        if(!depth_valid) {
-            compute_depth();
-        }
-        return depth;
-    }
-    bool has(char c) {
-        return children.count(c);
-    }
-    trie*& get(char c) {
-        return children[c];
-    }
-    trie* insert(const string_t& s, int i = string_start) {
-        sz++;
-        if(in_range(s, i)) {
-            (has(s[i]) ? get(s[i]) : get(s[i]) = new trie())->insert(s, i + string_move);
-            depth_valid = false;
+    trie* insert(const string_t& s, int i = i_start) {
+        size++;
+        if(0 <= i and i < s.size()) {
+            (get(s[i]) ? get(s[i]) : get(s[i]) = new trie())->insert(s, i + i_step);
+            height = max(height, get(s[i])->height + 1);
         } else {
-            n_end++;
+            n_ending++;
         }
         return this;
     }
-    trie* erase(const string_t& s, int i = string_start) {
-        sz--;
-        if(in_range(s, i)) {
-            assert(has(s[i]));
-            if(get(s[i])->erase(s, i + string_move)->size() == 0) {
-                remove(s[i]);
-            }
-            depth_valid = false;
+    trie* erase(const string_t& s, int i = i_start) {
+        size--;
+        if(0 <= i and i < s.size()) {
+            get(s[i])->erase(s, i + i_step)->size ? void() : remove(s[i]);
+            height = accumulate(children.begin(), children.end(), 0, [](int h, trie* t) {
+                return t ? max(h, t->height + 1) : h;
+            });
         } else {
-            n_end--;
+            n_ending--;
         }
         return this;
     }
     // count t ϵ T such that s = t
-    int count(const string_t& s, int i = string_start) {
-        return node(s, i) ? node(s, i)->n_end : 0;
+    int count(const string_t& s) {
+        auto t = (*this)[s];
+        return t ? t->n_ending : 0;
     }
     // count t ϵ T such that s is a prefix of t
-    int n_ext(const string_t& s, int i = string_start) {
-        return node(s, i) ? node(s, i)->size() : 0;
+    int n_suffixes(const string_t& s) {
+        auto t = (*this)[s];
+        return t ? t->size : 0;
     }
     // count t ϵ T such that t is a prefix of s
-    int n_pre(const string_t& s, int i = string_start) {
-        return n_end + (in_range(s, i) and has(s[i]) ? get(s[i])->n_pre(s, i + string_move) : 0);
+    int n_prefixes(const string_t& s) {
+        auto t = this;
+        int ans = t->n_ending;
+        for(int i = i_start; 0 <= i and i < s.size(); i += i_step) {
+            t = t ? t->get(s[i]) : nullptr;
+            ans += t ? t->n_ending : 0;
+        }
+        return ans;
     }
     // max LCP(s, t) for all t ϵ T
-    int lcp(const string_t& s, int i = string_start) {
-        return in_range(s, i) and has(s[i]) ? 1 + get(s[i])->lcp(s, i + string_move) : 0;
-    }
-    typedef trie<string_t, string_start, string_move> self_type;
-#ifdef DEBUG
-    template <typename Function>
-    void typecheck(const Function& f) {
-        using actual_type = typename callable_traits<Function>::function_type;
-        using expected_type = typename callable_traits<void(const pair<char, self_type*>&)>::function_type;
-        static_assert(is_same<actual_type, expected_type>::value, "trie traverse function must be void(const pair<char, trie*>&)");
-    }
-#endif
-    template <typename PreFunction, typename PostFunction, typename LeafFunction, typename CmpFunction>
-    void traverse(const PreFunction& pre, const PostFunction& post, const LeafFunction& leaf, const CmpFunction& cmp, char cur = 0) {
-    #ifdef DEBUG
-        typecheck(pre);
-        typecheck(post);
-        typecheck(leaf);
-        using actual_type = typename callable_traits<CmpFunction>::function_type;
-        using expected_type = typename callable_traits<bool(const pair<char, self_type*>&, const pair<char, self_type*>&)>::function_type;
-        static_assert(is_same<actual_type, expected_type>::value, "trie 'cmp' must be bool(const pair<char, trie*>, const pair<char, trie*>)");
-    #endif
-        vector<pair<char, self_type*>> to_proc;
-        for(auto& c : children) {
-            to_proc.push_back(c);
+    int lcp(const string_t& s) {
+        auto t = this;
+        int ans = 0;
+        for(int i = i_start; 0 <= i and i < s.size(); i += i_step) {
+            t = t ? t->get(s[i]) : nullptr;
+            ans += t ? 1 : 0;
         }
-        sort(to_proc.begin(), to_proc.end(), cmp);
-        for(const auto& p : to_proc) {
-            pre(p);
-            p.second->traverse(pre, post, leaf, cmp, p.first);
-            post(p);
-        }
-        if(to_proc.empty()) {
-            leaf({cur, this});
-        }
-    }
-    template <typename PreFunction, typename PostFunction, typename LeafFunction>
-    void traverse(const PreFunction& pre, const PostFunction& post, const LeafFunction& leaf, char cur = 0) {
-        traverse(pre, post, leaf, [](const pair<char, self_type*>& a, const pair<char, self_type*>& b) {
-            return a.first < b.first;
-        }, cur);
+        return ans;
     }
 };
