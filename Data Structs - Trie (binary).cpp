92cd67a29d81c043a01716c4b91ed15a7b7d2d68
Vernon Gutierrez <vern_go_123@yahoo.com>
2018-12-07 15:34:25 +0800

    Fix tries.

--- Data Structs - Trie (binary).cpp
+++ Data Structs - Trie (binary).cpp
@@ -1,40 +1,44 @@
 #include <bits/stdc++.h>
 #include "Data Structs - Trie.cpp"
 using namespace std;
-template <typename T> constexpr int n_bits() { return numeric_limits<T>::digits; }
-template <typename T> using binary_trie_base = trie<2, 0, bitset<n_bits<T>()>, n_bits<T>() - 1, -1>;
+template <typename T> struct int_iterator {
+    using type = T;
+    static constexpr int start = numeric_limits<T>::digits - 1, step = -1;
+    static constexpr int end(T x) { return -1; }
+    static inline bool at(T x, int i) { return x & (T(1) << i); }
+};
+template <typename T> using binary_trie_base = trie<2, 0, int_iterator<T>>;
 template <typename T> struct binary_trie : public binary_trie_base<T> {
-    using bitset_t = bitset<n_bits<T>()>;
-    void insert(T x) { binary_trie_base<T>::insert(bitset_t(x)); }
-    void erase(T x) { binary_trie_base<T>::erase(bitset_t(x)); }
-    int count(T x) { return binary_trie_base<T>::count(bitset_t(x)); }
-    T min_xor(const bitset_t& s) {
-        if(this->size == 0) {
-            return T(s.to_ullong());
+    using it = int_iterator<T>;
+    T min_xor(T x = 0) {
+        if(this->empty()) {
+            return x;
         } else {
             binary_trie_base<T>* t = this;
             T ans = 0;
-            for(int i = n_bits<T>() - 1; i >= 0; i--) {
-                ans += t->get(s[i]) ? 0 : T(1) << i;
-                t = t->get(s[i]) ? t->get(s[i]) : t->get(!s[i]);
+            for(int i = it::start; i != it::end(x); i += it::step) {
+                if(t->get(it::at(x, i))) {
+                    t = t->get(it::at(x, i));
+                } else {
+                    t = t->get(!it::at(x, i));
+                    ans += T(1) << i;
+                }
             }
             return ans;
         }
     }
-    T min_xor(T x = 0) { return min_xor(bitset_t(x)); }
     T max_xor(T x = 0) { return ~min_xor(~x); }
-    T mex(const bitset_t& s, int i = n_bits<T>() - 1) {
+    T mex(T x = 0) {
         binary_trie_base<T>* t = this;
         T ans = 0;
-        for(int i = n_bits<T>() - 1; i >= 0 and t and t->get(s[i]); i--) {
-            if(t->get(s[i])->size < T(1) << i) {
-                t = t->get(s[i]);
+        for(int i = it::start; i != it::end(x) and t and t->get(it::at(x, i)); i += it::step) {
+            if(t->get(it::at(x, i))->size < T(1) << i) {
+                t = t->get(it::at(x, i));
             } else {
-                t = t->get(!s[i]);
+                t = t->get(!it::at(x, i));
                 ans += T(1) << i;
             }
         }
         return ans;
     }
-    T mex(T x = 0) { return mex(bitset_t(x)); }
 };
--- Data Structs - Trie.cpp
+++ Data Structs - Trie.cpp
@@ -1,14 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <int alpha_size, char alpha_start, typename string_t = string, int i_start = 0, int i_step = 1>
+struct string_iterator {
+    using type = string;
+    static constexpr int start = 0, step = 1;
+    static inline int end(string s) { return s.size(); }
+    static inline char at(string s, int i) { return s[i]; }
+};
+template <int alpha_size, char alpha_start, typename it = string_iterator>
 struct trie {
     int size, n_ending, height;
-    vector<trie*> children;
-    trie() : size(0), n_ending(0), height(0), children(alpha_size) {}
-    ~trie() {
-        for(trie* t : children) {
-            delete t;
+    trie* children[alpha_size];
+    trie() : size(0), n_ending(0), height(0) {
+        for(trie*& t : children) {
+            t = nullptr;
         }
+    }
+    void clear() {
+        for(trie*& t : children) {
+            delete t;
+            t = nullptr;
+        }
+        size = 0, n_ending = 0, height = 0;
+    }
+    ~trie() {
+        clear();
+    }
+    bool empty() {
+        return size == 0;
     }
     trie*& get(char c) {
         return children[c - alpha_start];
@@ -16,10 +34,10 @@ struct trie {
     trie*& operator[](char c) {
         return get(c);
     }
-    trie* operator[](const string_t& s) {
+    trie* operator[](const typename it::type& s) {
         auto t = this;
-        for(int i = i_start; 0 <= i and i < s.size(); i += i_step) {
-            t = t ? t->get(s[i]) : nullptr;
+        for(int i = it::start; i != it::end(s); i += it::step) {
+            t = t ? t->get(it::at(s, i)) : nullptr;
         }
         return t;
     }
@@ -27,20 +45,20 @@ struct trie {
         delete children[c];
         children[c] = nullptr;
     }
-    trie* insert(const string_t& s, int i = i_start) {
+    trie* insert(const typename it::type& s, int i = it::start) {
         size++;
-        if(0 <= i and i < s.size()) {
-            (get(s[i]) ? get(s[i]) : get(s[i]) = new trie())->insert(s, i + i_step);
-            height = max(height, get(s[i])->height + 1);
+        if(i != it::end(s)) {
+            (get(it::at(s, i)) ? get(it::at(s, i)) : get(it::at(s, i)) = new trie())->insert(s, i + it::step);
+            height = max(height, get(it::at(s, i))->height + 1);
         } else {
             n_ending++;
         }
         return this;
     }
-    trie* erase(const string_t& s, int i = i_start) {
+    trie* erase(const typename it::type& s, int i = it::start) {
         size--;
-        if(0 <= i and i < s.size()) {
-            get(s[i])->erase(s, i + i_step)->size ? void() : remove(s[i]);
+        if(i != it::end(s)) {
+            get(it::at(s, i))->erase(s, i + it::step)->empty() ? remove(it::at(s, i)) : void();
             height = accumulate(children.begin(), children.end(), 0, [](int h, trie* t) {
                 return t ? max(h, t->height + 1) : h;
             });
@@ -50,31 +68,31 @@ struct trie {
         return this;
     }
     // count t ϵ T such that s = t
-    int count(const string_t& s) {
+    int count(const typename it::type& s) {
         auto t = (*this)[s];
         return t ? t->n_ending : 0;
     }
     // count t ϵ T such that s is a prefix of t
-    int n_suffixes(const string_t& s) {
+    int n_suffixes(const typename it::type& s) {
         auto t = (*this)[s];
         return t ? t->size : 0;
     }
     // count t ϵ T such that t is a prefix of s
-    int n_prefixes(const string_t& s) {
+    int n_prefixes(const typename it::type& s) {
         auto t = this;
         int ans = t->n_ending;
-        for(int i = i_start; 0 <= i and i < s.size(); i += i_step) {
-            t = t ? t->get(s[i]) : nullptr;
+        for(int i = it::start; i != it::end(s); i += it::step) {
+            t = t ? t->get(it::at(s, i)) : nullptr;
             ans += t ? t->n_ending : 0;
         }
         return ans;
     }
     // max LCP(s, t) for all t ϵ T
-    int lcp(const string_t& s) {
+    int lcp(const typename it::type& s) {
         auto t = this;
         int ans = 0;
-        for(int i = i_start; 0 <= i and i < s.size(); i += i_step) {
-            t = t ? t->get(s[i]) : nullptr;
+        for(int i = it::start; i != it::end(s); i += it::step) {
+            t = t ? t->get(it::at(s, i)) : nullptr;
             ans += t ? 1 : 0;
         }
         return ans;
