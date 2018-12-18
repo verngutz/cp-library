c6b06e345e6d73e2c16bb0b51f00c46ce0330ff7
Vernon Gutierrez <verngutz@gmail.com>
2018-12-19 02:25:48 +0800

    Fix binary search return types (for C++11).

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -4,7 +4,7 @@ template <typename Iter, typename T> int gt(Iter L, Iter R, T v) { return upper_
 template <typename Iter, typename T> int ge(Iter L, Iter R, T v) { return lower_bound(L, R, v) - L; }
 template <typename Iter, typename T> int lt(Iter L, Iter R, T v) { return lower_bound(L, R, v) - L - 1; }
 template <typename Iter, typename T> int le(Iter L, Iter R, T v) { return upper_bound(L, R, v) - L - 1; }
-template <typename Bst, typename T> auto gt(const Bst& bst, T v) { return bst.upper_bound(v); }
-template <typename Bst, typename T> auto ge(const Bst& bst, T v) { return bst.lower_bound(v); }
-template <typename Bst, typename T> auto lt(const Bst& bst, T v) { return bst.lower_bound(v) == bst.begin() ? bst.end() : --bst.lower_bound(v); }
-template <typename Bst, typename T> auto le(const Bst& bst, T v) { return bst.upper_bound(v) == bst.begin() ? bst.end() : --bst.upper_bound(v); }
+template <typename Bst, typename T> typename Bst::iterator gt(const Bst& bst, T v) { return bst.upper_bound(v); }
+template <typename Bst, typename T> typename Bst::iterator ge(const Bst& bst, T v) { return bst.lower_bound(v); }
+template <typename Bst, typename T> typename Bst::iterator lt(const Bst& bst, T v) { return bst.lower_bound(v) == bst.begin() ? bst.end() : --bst.lower_bound(v); }
+template <typename Bst, typename T> typename Bst::iterator le(const Bst& bst, T v) { return bst.upper_bound(v) == bst.begin() ? bst.end() : --bst.upper_bound(v); }
