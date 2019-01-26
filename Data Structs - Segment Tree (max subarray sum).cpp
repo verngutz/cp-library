8fc956d657ef1e9a065f11b588a3126329c68244
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-01-27 02:16:38 +0800

    Add maximum subarray sum segment tree.

---
+++ Data Structs - Segment Tree (max subarray sum).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "Data Structs - Segment Tree (compact).cpp"
+using namespace std;
+using ll = long long;
+struct mss_node { ll max_subarray, all, prefix, suffix; };
+template <ll INF> struct mss_segtree : public segtree<mss_node> {
+    mss_segtree(int size) : segtree<mss_node>({-INF, 0, -INF, -INF}, [](auto L, auto R) {
+        return mss_node{
+            max({L.max_subarray, R.max_subarray, L.suffix + R.prefix}),
+            L.all + R.all,
+            max(L.prefix, L.all + R.prefix),
+            max(R.suffix, R.all + L.suffix)
+        };
+    }, size) {}
+    void update(int i, ll x) {
+        segtree<mss_node>::update(i, {x, x, x, x});
+    }
+};
