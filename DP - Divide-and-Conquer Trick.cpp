0c87d9ed8fc84b88682e0e36139f75fc6a46271f
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-03-26 21:49:56 +0800

    Improve Divide-and-Conquer Trick

--- DP - Divide-and-Conquer Trick.cpp
+++ DP - Divide-and-Conquer Trick.cpp
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 // DP on partitions: optimum partition of array of size SIZE into PARTS cells
-// cost of putting elements from index i to index j into one cell is c(i, j)
-// solve recurrence of form OPT(p, j) = min i < j OPT(p - 1, i) + c(i, j)
+// cost of putting elements from index i to index j into one cell, the pth one from the left, is c(p, i, j)
+// solve recurrence of form OPT(p, j) = min i <= j OPT(p - 1, i - 1) + c(p, i, j)
+// change cmp to greater<T>() for maximization
 // argmin(p, j) <= argmin(p, j + 1) must be satisfied
-// alternatively, c must satisfy for all w <= x <= y <= z:
-// quadrangle inequality: c(w, y) + c(x, z) <= c(w, z) + c(x, y)
+// alternatively, c must satisfy for all w <= x <= y <= z and all p:
+// quadrangle inequality: c(p, w, y) + c(p, x, z) <= c(p, w, z) + c(p, x, y)
 // runs in O(PARTS SIZE lg SIZE) assuming c is constant time
-template<typename T, size_t PARTS, size_t SIZE>
-T dnc(int parts, int n, const function<T(int, int)>& c) {
-    static T opt[PARTS + 1][SIZE + 1];
-    static int arg[PARTS + 1][SIZE + 1];
+template<typename T, size_t PARTS, size_t SIZE, bool SAVE_SPACE = true>
+T dnc(int parts, int n, const function<T(int, int, int)>& c,
+function<bool(T, T)> cmp = less<T>(), bool allow_empty = false, T zero = 0) {
+    static T opt[SAVE_SPACE ? 2 : (PARTS + 1)][SIZE + 1];
+    static int arg[SAVE_SPACE ? 2 : (PARTS + 1)][SIZE + 1];
     function<void(int, int, int, int, int)> make_row = [&](int p, int s, int e, int ks, int ke) {
         if(s > e) return;
+        int curr_row = SAVE_SPACE ? p & 1 : p;
+        int prev_row = SAVE_SPACE ? (p - 1) & 1 : (p - 1);
         int m = (s + e) / 2;
-        for(int k = ks; k <= min(ke, m - 1); k++) {
-            if(k == ks or opt[p][m] > opt[p - 1][k] + c(k + 1, m)) {
-                arg[p][m] = k, opt[p][m] = opt[p - 1][k] + c(k + 1, m);
+        for(int k = ks; k <= min(ke, m - not allow_empty); k++) {
+            if(k == ks or cmp(opt[prev_row][k] + c(p, k + 1, m), opt[curr_row][m])) {
+                opt[curr_row][m] = opt[prev_row][k] + c(p, k + 1, m);
+                arg[curr_row][m] = k;
             }
         }
-        make_row(p, s, m - 1, ks, arg[p][m]);
-        make_row(p, m + 1, e, arg[p][m], ke);
+        make_row(p, s, m - 1, ks, arg[curr_row][m]);
+        make_row(p, m + 1, e, arg[curr_row][m], ke);
     };
     for(int p = 1; p <= parts; p++) {
         if(p == 1) {
             for(int i = 0; i <= n; i++) {
-                opt[p][i] = c(1, i);
+                opt[SAVE_SPACE ? p & 1 : p][i] = i ? c(p, 1, i) : zero;
             }
         } else {
             make_row(p, 0, n, 0, n);
         }
     }
-    return opt[parts][n];
+    return opt[SAVE_SPACE ? parts & 1 : parts][n];
 }
