d9fe24150bf6c027d41c646442952d071c7cce22
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-06-18 20:00:53 +0800

    remove kg generators

--- template/formatter.py
+++
@@ -1,7 +0,0 @@
-# print the data into the correct input format
-
-def print_to_file(file, cases):
-    print(len(cases), file=file)
-    for arr in cases:
-        print(len(arr), file=file)
-        print(*arr, file=file)
