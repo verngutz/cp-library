815a2ca0d9e25c9e4d31c6ccc037666824f59a88
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-07-07 23:55:30 +0800

    Improve hashing.

--- Strings - Hashing.cpp
+++ Strings - Hashing.cpp
@@ -7,50 +7,63 @@ seed_seq seq {
     (uint64_t) (uintptr_t) make_unique<char>().get()
 };
 mt19937 rng(seq);
-const uint64_t M1 = (1ull << 61) - 1;
-const uint64_t M2 = (1ull << 31) - 1;
-const uint64_t X1 = uniform_int_distribution<uint64_t>(256, M1-1)(rng);
-const uint64_t X2 = uniform_int_distribution<uint64_t>(256, M2-1)(rng);
-uint64_t modadd(uint64_t a, uint64_t b, uint64_t M) {
-    return a + b >= M ? a + b - M : a + b;
-}
-uint64_t modmul1(uint64_t a, uint64_t b) {
-	uint64_t l1 = (uint32_t)a, h1 = a >> 32, l2 = (uint32_t)b, h2 = b >> 32;
-	uint64_t l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2;
-	uint64_t ret = (l & M1) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
-	ret = (ret & M1) + (ret >> 61);
-	ret = (ret & M1) + (ret >> 61);
-	return ret - 1;
-}
-uint64_t modmul2(uint64_t a, uint64_t b) {
-    uint64_t ret = a * b + 1;
-    ret = (ret & M2) + (ret >> 31);
-    ret = (ret & M2) + (ret >> 31);
-    return ret - 1;
-}
-struct hash_struct {
-    uint64_t M;
-    vector<uint64_t> prefix, mpow;
-    const function<uint64_t(uint64_t, uint64_t)> modmul;
-    hash_struct(uint64_t M, int n, const function<uint64_t(uint64_t, uint64_t)>& modmul)
-    : M(M), prefix(n), mpow(n), modmul(modmul) {}
+template <typename T> struct rolling_hash {
+    static uint64_t add(uint64_t a, uint64_t b) { return a + b >= T::M ? a + b - T::M : a + b; }
+    static uint64_t sub(uint64_t a, uint64_t b) { return T::add(a, T::M - b); }
+    static uint64_t mul(uint64_t, uint64_t);
+    static inline uint64_t X = uniform_int_distribution<uint64_t>(256, T::M - 1)(rng);
+    static inline vector<uint64_t> mpow;
+    static void resize(int n) {
+        T::mpow.reserve(n + 1);
+        while(T::mpow.size() <= n) {
+            T::mpow.push_back(T::mpow.empty() ? 1 : T::mul(T::mpow.back(), T::X));
+        }
+    }
+    vector<uint64_t> prefix;
+    rolling_hash() {}
+    rolling_hash(const string& s) { append(s); }
+    uint64_t query(int L, int R) {
+        assert(L <= R), assert(R <= prefix.size());
+        resize(R - L);
+        return L ? T::sub(prefix[R - 1], T::mul(prefix[L - 1], T::mpow[R - L])) : prefix[R - 1];
+    }
+    T& append(char c) {
+        prefix.push_back(prefix.empty() ? c : T::add(T::mul(prefix.back(), T::X), c));
+        return (T&)*this;
+    }
+    T& append(const string& s) {
+        prefix.reserve(prefix.size() + s.size());
+        for(const char& c : s) {
+            push_back(c);
+        }
+        return (T&)*this;
+    }
+    T& operator+=(char c) { return append(c); }
+    T& operator+=(const string& s) { return append(s); }
+    void push_back(char c) { append(c); }
+    void pop_back() { prefix.pop_back(); }
 };
-hash_struct make_prefix_hash1(string& s) {
-    hash_struct h(M1, s.size(), modmul1);
-    for(int i = 0; i < s.size(); i++) {
-        h.prefix[i] = modadd(modmul1(i ? h.prefix[i - 1] : 0, X1), s[i], M1);
-        h.mpow[i] = i ? modmul1(h.mpow[i - 1], X1) : 1;
+struct hash1 : public rolling_hash<hash1> {
+    static const uint64_t M = (1ull << 61) - 1;
+    static uint64_t mul(uint64_t a, uint64_t b) {
+        uint64_t l1 = (uint32_t)a, h1 = a >> 32, l2 = (uint32_t)b, h2 = b >> 32;
+        uint64_t l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2;
+        uint64_t ret = (l & M) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
+        ret = (ret & M) + (ret >> 61);
+        ret = (ret & M) + (ret >> 61);
+        return ret - 1;
     }
-    return h;
-}
-hash_struct make_prefix_hash2(string& s) {
-    hash_struct h(M2, s.size(), modmul2);
-    for(int i = 0; i < s.size(); i++) {
-        h.prefix[i] = modadd(modmul2(i ? h.prefix[i - 1] : 0, X2), s[i], M2);
-        h.mpow[i] = i ? modmul2(h.mpow[i - 1], X2) : 1;
+    hash1() {}
+    hash1(const string& s) : rolling_hash<hash1>(s) {}
+};
+struct hash2 : public rolling_hash<hash2> {
+    static const uint64_t M = (1ull << 31) - 1;
+    static uint64_t mul(uint64_t a, uint64_t b) {
+        uint64_t ret = a * b + 1;
+        ret = (ret & M) + (ret >> 31);
+        ret = (ret & M) + (ret >> 31);
+        return ret - 1;
     }
-    return h;
-}
-uint64_t get_substring_hash(hash_struct& h, int L, int R) {
-    return R >= L ? modadd(h.prefix[R], (L ? h.M - h.modmul(h.mpow[R - L + 1], h.prefix[L - 1]) : 0), h.M) : 0;
-}
+    hash2() {}
+    hash2(const string& s) : rolling_hash<hash2>(s) {}
+};
