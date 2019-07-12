836910d3a0893d87b5548e3fd584d10e5d7b33ab
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-07-13 04:08:24 +0800

    Minor fix

--- Number Theory - Factorization O(sqrt n).cpp
+++ Number Theory - Factorization O(sqrt n).cpp
@@ -2,8 +2,10 @@
 using namespace std;
 template <typename T, typename TEachDivisor, typename TEachPrime>
 constexpr void factorize(T n, TEachDivisor each_divisor, TEachPrime each_prime = [](T) {}) {
-    static_assert(is_convertible<decltype(each_divisor), function<void(T)>>::value, "each_divisor must be void(T)");
-    static_assert(is_convertible<decltype(each_prime), function<void(T)>>::value, "each_prime must be void(T)");
+    static_assert(is_convertible<decltype(each_divisor), function<void(T)>>::value,
+        "each_divisor must be void(T)");
+    static_assert(is_convertible<decltype(each_prime), function<void(T)>>::value,
+        "each_prime must be void(T)");
     for(T i = 2; i * i <= n; i++) {
         if(n % i == 0) {
             each_prime(i);
