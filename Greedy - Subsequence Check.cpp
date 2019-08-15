51559637b3b7de50130c97fe39a9368ee9157272
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-08-16 07:30:57 +0800

    Fix subsequence check

--- Greedy - Subsequence Check.cpp
+++ Greedy - Subsequence Check.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool subseq(const string& s, const string& t, int i = 0, int j = 0) {
-    for(int i = 0, j = 0; i < s.length() and j < t.length(); i++) {
-        j += s[i] == t[j];
+    for(; i < s.length() and j < t.length(); j++) {
+        i += s[i] == t[j];
     }
-    return j == t.length();
+    return i == s.length();
 }
