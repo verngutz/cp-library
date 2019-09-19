f712d6f383645f76c970712b4e3dc1b10e531199
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-09-20 07:20:47 +0800

    minor aesthetic fixes

--- Math - Fast Pow.cpp
+++ Math - Fast Pow.cpp
@@ -1,10 +1,9 @@
+#ifndef __FASTPOW_INCLUDED_
+#define __FASTPOW_INCLUDED_
 using ll = long long;
 template <typename T> constexpr T fpow_eq(T& x, ll y, T identity = 1) {
-    for(; y > 0; x *= x, y >>= 1) {
-        if(y & 1) {
-            identity *= x;
-        }
-    }
+    for(; y > 0; x *= x, y >>= 1) if(y & 1) identity *= x;
     return x = identity;
 }
 template <typename T> constexpr T fpow(T x, ll y, T identity = 1) { return fpow_eq(x, y, identity); }
+#endif
