c67cc2a00b9dcf849dbc8c814516b2d515a718d9
Vernon Gutierrez <vern_go_123@yahoo.com>
2019-09-20 11:03:22 +0800

    optimize sigma transform

--- Convolutions (Bitwise) - Mobius Transform.cpp
+++ Convolutions (Bitwise) - Mobius Transform.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 template <typename T> vector<T>& sigma(vector<T>& a) {
     for(int i = 0; i < a.size(); i++) {
-        if(__builtin_popcount(i) % 2) {
-            a[i] = -a[i];
-        }
+        a[i] *= -2 * (__builtin_popcount(i) % 2) + 1;
     }
     return a;
 }
