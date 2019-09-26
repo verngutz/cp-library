5c6c41b0cc703dcb24861496aca8bb6ac6c2bf85
Vernon Gutierrez <verngutz@gmail.com>
2019-09-26 20:56:24 +0800

    fix subtree convolution

--- DP - Subtree Convolution.cpp
+++ DP - Subtree Convolution.cpp
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
-#include "Graph - Adjacency List (unweighted).cpp"
-#include "Graph - DFS.cpp"
+#include "Graph Structures - Edge (unweighted).cpp"
+#include "Graph Structures - Incidence List.cpp"
+#include "Graph Algos - DFS.cpp"
 using namespace std;
-
-template <typename T, size_t N, size_t K>
-T tree_convolution(T zero, const function<T(T, T)>& add, const function<T(T, T)>& mul, graph<0>& g, int k) {
-    vector<int> p(g.n + 1), sz(g.n + 1, 1), topological_order;
-    dfs(g, [](int, int) {}, [&](int u, int from) {
-        p[u] = from;
-        topological_order.push_back(u);
+template <typename T, int N, int K, bool Index, typename Add = decltype(plus<T>()), typename Mul = decltype(multiplies<T>())>
+T(*tree_convolution(graph<0, edge, Index>& g, bool connected, T zero = 0, Add&& add = plus<T>(), Mul&& mul = multiplies<T>()))[K+1] {
+    vector<int> p(g.adj.size()), sz(g.adj.size(), 1), topological_order;
+    dfs(g, [&](dfs_params<edge>& psetter) {
+        psetter.pos_node = [&](int u) { topological_order.push_back(u); };
+        psetter.pos_edge = [&](edge& e) { p[e.v] = e.u; };
     });
-    static T OPT[N+1][K+1];
-    fill_n(&OPT[0][0], (N+1)*(K+1), zero);
+    static T opt[N+1][K+1];
+    fill_n(&opt[0][0], (N+1)*(K+1), zero);
     for(int v : topological_order) {
-        // write base cases here
+        // base cases
     }
     topological_order.pop_back();
     for(int v : topological_order) {
         int u = p[v];
-        for(int k_par = min(k, sz[v] + sz[u]); k_par >= 0; k_par--) {
+        for(int k_par = min(K, sz[v] + sz[u]); k_par >= connected; k_par--) {
             T ans = zero;
-            for(int k_sub = max(0, k_par - sz[u]); k_sub <= min(sz[v], k_par); k_sub++) {
-                // write convolution formula here
-                ans = add(ans, mul(OPT[u][k_par - k_sub], OPT[v][k_sub]));
+            for(int k_sub = max(0, k_par - sz[u]); k_sub <= min(sz[v], k_par - connected); k_sub++) {
+                ans = add(ans, mul(opt[u][k_par - k_sub], opt[v][k_sub] /* + cost(u, v) */));
             }
-            OPT[u][k_par] = ans;
+            opt[u][k_par] = ans;
         }
         sz[u] += sz[v];
     }
-    return OPT[1][k];
+    return opt;
 }
