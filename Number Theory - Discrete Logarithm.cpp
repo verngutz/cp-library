185eca9ca19edbca698562047a04a90d7d042ff4
Vernon Gutierrez <verngutz@gmail.com>
2020-03-28 22:36:42 +0800

    fixed discrete logarithm

--- Number Theory - Discrete Logarithm.cpp
+++ Number Theory - Discrete Logarithm.cpp
@@ -2,17 +2,27 @@
 #include "Number Theory - Mod Pow.cpp"
 #include "Number Theory - Totient.cpp"
 using namespace std;
-// find all/one x (mod M) such that a^x = b (mod c), or log_a b = x (mod c)
+// find all/one x (mod c) such that a^x = b (mod c), or log_a b = x (mod c)
 vector<ll> dlog(ll a, ll b, ll c, bool find_one = false) {
     a %= c, b %= c;
     ll offset = 0, g;
     while((g = __gcd(a, c)) > 1 and b % g == 0) {
         c /= g;
-        b = int(b / g * mpow(a / g, phi(c) - 1, c));
+        b = b / g * mpow(a / g, phi(c) - 1, c) % c;
         offset++;
     }
     vector<ll> ans;
-    if(a > 0 and g == 1) {
+    if(a == 0 and b == 0) {
+        for(ll x = 1; x <= c; x++) {
+            ans.push_back(1 + offset);
+            if(find_one) return ans;
+        }
+    } else if(b == 1) {
+        ans.push_back(offset);
+        if(find_one) return ans;
+    }
+    if(b % g == 0) {
+        b %= c;
         ll n = ll(sqrt(c) + 1), f1 = 1, f2 = 1;
         unordered_map<ll, vector<ll>> m;
         for(ll q = 0; q <= n; q++) {
@@ -28,12 +38,10 @@ vector<ll> dlog(ll a, ll b, ll c, bool find_one = false) {
             if(m.count(f2)) {
                 for(ll q : m[f2]) {
                     ans.push_back(n * p - q + offset);
+                    if(find_one) return ans;
                 }
-                if(find_one) break;
             }
         }
-    } else if(b == 1 or c == 1) {
-        ans.push_back(offset);
     }
     return ans;
 }
