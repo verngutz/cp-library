f3210af0b169db7d61f7fe30b6a58a60346c7ac5
Vernon Gutierrez <verngutz@gmail.com>
2020-05-30 13:24:45 +0800

    style fixes

--- Convolutions (Bitwise) - Covering Power.cpp
+++ Convolutions (Bitwise) - Covering Power.cpp
@@ -2,7 +2,7 @@
 #include "Convolutions (Bitwise) - Covering Product.cpp"
 #include "Math - Fast Pow.cpp"
 using namespace std;
-template <typename T> vector<T> operator^=(vector<T>& x, long long y) {
+template <typename T> vector<T>& operator^=(vector<T>& x, long long y) {
     zeta(x);
     transform(x.begin(), x.end(), x.begin(), [y](T& xi) { return fpow(xi, y); });
     return mu(x);
