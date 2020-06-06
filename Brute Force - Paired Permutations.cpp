807a68268695d53bf8fc5e347b0d56e3fed2c9ac
Vernon Gutierrez <verngutz@gmail.com>
2020-06-06 20:49:27 +0800

    remove std::function overhead

--- Brute Force - Paired Permutations.cpp
+++ Brute Force - Paired Permutations.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
-#include "callable.hpp/callable.hpp"
 using namespace std;
 // go through all ways to form unordered pairs from [0, n - 1]
 // f is the action to do on every such pairing p
 // p[0] is paired with p[n-1], p[1] with p[n-2], etc.
 // runs in n (n - 2) (n - 4) ... time
-void paired_permutations(int n, const function<void(vector<int>&)>& f) {
+template <typename F> void paired_permutations(int n, const F& f) {
+    static_assert(is_convertible<decltype(f), void(vector<int>&)>::value, "f must be void(vector<int>&)");
     vector<int> p(n);
     iota(p.begin(), p.end(), 0);
     function<void(int)> rb = [&](int i) {
