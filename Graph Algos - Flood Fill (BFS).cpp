65c1438ae254ec2d93e3b524b786a74c9514e353
Vernon Gutierrez <verngutz@gmail.com>
2020-06-06 20:42:16 +0800

    fixed/simplified flood fill

--- Graph Algos - Flood Fill (BFS).cpp
+++ Graph Algos - Flood Fill (BFS).cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 #include "Data Structs - Grid.cpp"
 using namespace std;
-template <typename signature> using f = const function<signature>;
-f<bool(int, int)> hori = [](int di, int dj) { return abs(di) == 0 and abs(dj) == 1; };
-f<bool(int, int)> vert = [](int di, int dj) { return abs(di) == 1 and abs(dj) == 0; };
-f<bool(int, int)> diag = [](int di, int dj) { return abs(di) == 1 and abs(dj) == 1; };
-f<bool(int, int)> operator||(f<bool(int, int)>& f1, f<bool(int, int)>& f2) {
-    return [&](int di, int dj) { return f1(di, dj) or f2(di, dj); };
-}
-template <typename Can> grid<int> flood(grid<int>& a, vector<pair<int, int>>& s, Can&& can) {
-    static_assert(is_convertible<decltype(can), function<bool(int, int)>>::value, "can must be bool(int, int)");
-    grid<int> d(a.n, a.m, -1);
+vector<pair<int, int>> hori = {{0, -1}, {0, 1}};
+vector<pair<int, int>> vert = {{-1, 0}, {1, 0}};
+vector<pair<int, int>> diag = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+vector<pair<int, int>>& operator+=(vector<pair<int, int>>& a, const vector<pair<int, int>>& b) {
+    a.insert(a.end(), b.begin(), b.end());
+    return a;
+};
+vector<pair<int, int>> operator+(vector<pair<int, int>> a, const vector<pair<int, int>>& b) {
+    return a += b;
+};
+grid<int> flood(const grid<int>& a, const vector<pair<int, int>>& s, const vector<pair<int, int>>& moves) {
+    grid<int> d(a.n, a.m, lim<int>::max());
+    queue<pair<int, int>> q;
     for(auto [i, j] : s) {
         d[i][j] = 0;
+        q.push({i, j});
     }
-    queue<pair<int, int>> q(s.begin(), s.end());
     while(not q.empty()) {
         auto [i, j] = q.front(); q.pop();
-        for(int di = -1; di <= 1; di++) {
-            for(int dj = -1; dj <= 1; dj++) {
-                int ni = i + di, nj = j + dj;
-                if(0 <= ni and ni < a.n and 0 <= nj and nj < a.m and a[ni][nj] and d[ni][nj] == -1 and can(di, dj)) {
-                    d[ni][nj] = d[i][j] + 1;
-                    q.push({ni, nj});
-                }
+        for(auto [di, dj] : moves) {
+            int ni = i + di, nj = j + dj;
+            if(0 <= ni and ni < a.n and 0 <= nj and nj < a.m and a[ni][nj] and d[ni][nj] > d[i][j] + 1) {
+                d[ni][nj] = d[i][j] + 1;
+                q.push({ni, nj});
             }
         }
     }
--- Graph Algos - Flood Fill (DFS).cpp
+++ Graph Algos - Flood Fill (DFS).cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 #include "Data Structs - Grid.cpp"
 using namespace std;
-template <typename signature> using f = const function<signature>;
-f<bool(int, int)> hori = [](int di, int dj) { return abs(di) == 0 and abs(dj) == 1; };
-f<bool(int, int)> vert = [](int di, int dj) { return abs(di) == 1 and abs(dj) == 0; };
-f<bool(int, int)> diag = [](int di, int dj) { return abs(di) == 1 and abs(dj) == 1; };
-f<bool(int, int)> operator||(f<bool(int, int)>& f1, f<bool(int, int)>& f2) {
-    return [&](int di, int dj) { return f1(di, dj) or f2(di, dj); };
-}
-template <typename Can> grid<int> flood(grid<int>& a, Can&& can) {
-    static_assert(is_convertible<decltype(can), function<bool(int, int)>>::value, "can must be bool(int, int)");
+vector<pair<int, int>> hori = {{0, -1}, {0, 1}};
+vector<pair<int, int>> vert = {{-1, 0}, {1, 0}};
+vector<pair<int, int>> diag = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+vector<pair<int, int>>& operator+=(vector<pair<int, int>>& a, const vector<pair<int, int>>& b) {
+    a.insert(a.end(), b.begin(), b.end());
+    return a;
+};
+vector<pair<int, int>> operator+(vector<pair<int, int>> a, const vector<pair<int, int>>& b) {
+    return a += b;
+};
+grid<int> flood(grid<int>& a, const vector<pair<int, int>>& moves) {
     grid<int> component(a.n, a.m);
     int component_id = 0;
     function<void(int, int)> dfs = [&](int i, int j) {
         if(0 <= i and i < a.n and 0 <= j and j < a.m and a[i][j] and not component[i][j]) {
             component[i][j] = component_id;
-            for(int di = -1; di <= 1; di++) {
-                for(int dj = -1; dj <= 1; dj++) {
-                    if(can(di, dj)) {
-                        dfs(i + di, j + dj);
-                    }
-                }
+            for(auto [di, dj] : moves) {
+                dfs(i + di, j + dj);
             }
         }
     };
