df6fce8a480a39030696311828d4c144f79f6bc4
Vernon Gutierrez <verngutz@gmail.com>
2020-06-06 20:43:30 +0800

    fixed bfs

--- Graph Algos - BFS.cpp
+++ Graph Algos - BFS.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 #include "Graph Structures - Incidence List.cpp"
+template <typename T> using lim = numeric_limits<T>;
 template <bool Directed, typename TEdge, bool Index>
-pair<vector<int>, vector<int>> bfs(graph<Directed, TEdge, Index>& g, const vector<int>& s) {
-    vector<int> d(g.adj.size(), -1), p(g.adj.size(), -1);
+pair<vector<int>, vector<int>> bfs(const graph<Directed, TEdge, Index>& g, const vector<int>& s) {
+    vector<int> d(g.adj.size(), lim<int>::max()), p(g.adj.size(), -1);
     queue<int> q;
     for(int u : s) {
         d[u] = 0;
@@ -11,8 +12,8 @@ pair<vector<int>, vector<int>> bfs(graph<Directed, TEdge, Index>& g, const vecto
     while(not q.empty()) {
         int u = q.front(); q.pop();
         for(int e : g[u]) {
-            if(d[g(e).v] == -1) {
-                d[g(e).v] = d[g(e).u] + 1, p[g(e).v] = e;
+            if(d[g(e).v] > d[u] + 1) {
+                d[g(e).v] = d[u] + 1, p[g(e).v] = e;
                 q.push(g(e).v);
             }
         }
