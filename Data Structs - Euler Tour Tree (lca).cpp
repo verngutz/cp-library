5f6f611a802319f5d94704365e0cdfbded5bdb96
Vernon Gutierrez <vern_go_123@yahoo.com>
2020-12-03 23:52:33 +0800

    fixed ett

--- Data Structs - Euler Tour Tree (lca).cpp
+++ Data Structs - Euler Tour Tree (lca).cpp
@@ -17,7 +17,16 @@ template <typename TEdge, bool Index> struct ett_lca {
         }
         return sparse_table<pii>(depthed_tour, {{lim<int>::max(), 0}, [](const pii& x, const pii& y) { return min(x, y); }});
     }
+    bool is_ancestor(int u, int v) {
+        return d[u] <= d[v] and f[v] <= f[u];
+    }
     int lca(int u, int v) {
-        return u == v ? u : (f[u] < d[v] ? st.query(f[u], d[v], true) : st.query(f[v], d[u], true)).second;
+        if(is_ancestor(u, v)) {
+            return u;
+        } else if(is_ancestor(v, u)) {
+            return v;
+        } else {
+            return (f[u] < d[v] ? st.query(f[u], d[v], true) : st.query(f[v], d[u], true)).second;
+        }
     }
 };
