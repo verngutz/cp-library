e583e3dbbac01a94f59e33059950676bc2391728
Vernon Gutierrez <vern_go_123@yahoo.com>
2021-03-17 05:51:16 +0800

    z function edge case

--- Strings - Z Function.cpp
+++ Strings - Z Function.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 // z[i] is the length of the longest common prefix between s and the suffix of s starting at i
 vector<int> make_z_function(const string& s) {
-    vector<int> z(s.length());
+    vector<int> z(s.length() + 1);
     z[0] = s.length();
     for(int i = 1, L = 0, R = 0; i < s.length(); i++) {
         z[i] = i < R ? min(z[i - L], R - i) : 0;
