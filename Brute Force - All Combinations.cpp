595d3643f3d3f4a2257faff4ad39a89f92cabc02
Vernon Gutierrez <vern_go_123@yahoo.com>
2021-03-17 05:47:56 +0800

    brute force all combinations

---
+++ Brute Force - All Combinations.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename F> void combinations(int n, int k, const F& f) {
+    static_assert(is_convertible<decltype(f), function<void(vector<int>&)>>::value, "f must be void(vector<int>&)");
+    function<void(int, vector<int>&)> rb = [&](int i, vector<int>& c) {
+        if(c.size() == k) {
+            f(c);
+        } else if(i < n) {
+            if(n - i + c.size() > k) {
+                rb(i + 1, c);
+            }
+            c.push_back(i);
+            rb(i + 1, c);
+            c.pop_back();
+        }
+    };
+    static vector<int> c;
+    c.reserve(k);
+    rb(0, c);
+}
