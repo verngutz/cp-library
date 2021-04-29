e255b596c26cc8902ffe98249b4c1eb33e3a13a6
Vernon Gutierrez <vern_go_123@yahoo.com>
2021-04-29 20:13:44 +0800

    complete graph generator

---
+++ Stress Testing - Generate Complete Graph.py
@@ -0,0 +1,6 @@
+def make_complete_graph(n, m):
+        edges = []
+        for i in range(1, n + 1):
+            for j in range(i + 1, n + 1):
+                edges.append(rand.choice([(i, j), (j, i)]))
+        return edges
