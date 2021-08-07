e9872210e27c1cb92ae277193af2a4acf6f14a4f
Vernon Gutierrez <vern_go_123@yahoo.com>
2021-08-07 23:12:24 +0800

    fix max area rectangle under histogram

--- Misc - Max Area Rectangle Under Histogram.cpp
+++ Misc - Max Area Rectangle Under Histogram.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 #include "Data Structs - All Nearest Smaller Values.cpp"
 using namespace std;
-using ll = long long;
-ll max_histogram_rectangle(const vector<ll>& a) {
-    vector<int> left_nearest = left_nearest_smaller_values(a);
-    vector<int> right_nearest = right_nearest_smaller_values(a);
-    ll ans = 0;
+template <typename T> T max_histogram_rectangle(const vector<T>& a) {
+    vector<int> left_nearest = lnsv(a);
+    vector<int> right_nearest = rnsv(a);
+    T ans = 0;
     for(int i = 0; i < a.size(); i++) {
         ans = max(ans, a[i] * (right_nearest[i] - left_nearest[i] - 1));
     }
