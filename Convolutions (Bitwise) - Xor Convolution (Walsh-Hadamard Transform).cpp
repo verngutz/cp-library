53df505aba1b5f0deaa481a8784aa92428bd83a5
Vernon Gutierrez <vern_go_123@yahoo.com>
2021-08-07 23:13:42 +0800

    optimize xor convolution and add exponentiation

--- Convolutions (Bitwise) - Xor Convolution (Walsh-Hadamard Transform).cpp
+++ Convolutions (Bitwise) - Xor Convolution (Walsh-Hadamard Transform).cpp
@@ -1,26 +1,37 @@
 #include <bits/stdc++.h>
+#include "Math - Fast Pow.cpp"
 using namespace std;
-template <typename T>
-vector<T>& fwht(vector<T>& a) {
-    int n = a.size();
-    if(n == 1) {
-        return a;
-    } else {
-        vector<T> s(a.begin(), a.begin() + n / 2), e(a.begin() + n / 2, a.end());
-        fwht(s), fwht(e);
-        for(int j = 0; j < n; j++) {
-            a[j] = s[j % (n / 2)] + (j < n / 2 ? 1 : -1) * e[j % (n / 2)];
+template <typename T> vector<T>& fwht(vector<T>& a, int s, int e) {
+    if(e - s > 1) {
+        int m = (s + e) / 2;
+        fwht(a, s, m);
+        fwht(a, m, e);
+        for(int i = s, j = m; i < m; i++, j++) {
+            int tmp = a[i] + a[j];
+            a[j] = a[i] - a[j], a[i] = tmp;
         }
-        return a;
     }
+    return a;
 }
-template <typename T> vector<T>& operator^=(vector<T>& a, vector<T>& b) {
+template <typename T> vector<T>& fwht(vector<T>& a) {
+    return fwht(a, 0, a.size());
+}
+template <typename T> vector<T>& operator*=(vector<T>& a, vector<T>& b) {
     fwht(a);
     if(addressof(a) != addressof(b)) fwht(b);
     assert(a.size() == b.size());
     transform(a.begin(), a.end(), b.begin(), a.begin(), multiplies<T>());
-    transform(a.begin(), a.end(), a.begin(), [n = a.size()](T x) { return x / n; });
-    if(addressof(a) != addressof(b)) fwht(b);
-    return a;
+    transform(a.begin(), a.end(), a.begin(), [&](const T& x) { return x / int(a.size()); });
+    if(addressof(a) != addressof(b)) {
+        fwht(b);
+        transform(b.begin(), b.end(), b.begin(), [&](const T& x) { return x / int(b.size()); });
+    }
+    return fwht(a);
 }
-template <typename T> vector<T> operator^(vector<T> a, vector<T>& b) { return a ^= b; }
+template <typename T> vector<T> operator*(vector<T> a, vector<T>& b) { return a *= b; }
+template <typename T> vector<T>& operator^=(vector<T>& x, long long y) {
+    fwht(x);
+    transform(x.begin(), x.end(), x.begin(), [&](T& xi) { return fpow_eq(xi, y) / int(x.size()); });
+    return fwht(x);
+}
+template <typename T> vector<T> operator^(vector<T> x, long long y) { return x ^= y; }
