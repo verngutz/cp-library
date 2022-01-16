21a3d4e1734abdd2eada580a87e7c4cf4cea1872
Vernon Gutierrez <verngutz@gmail.com>
2022-01-16 14:14:34 +0800

    speed up factorials and inverse factorials

--- Combinatorics - Permutations.cpp
+++ Combinatorics - Permutations.cpp
@@ -1,15 +1,21 @@
 #include "Math - Modular Arithmetic.cpp"
 template <typename T = mint> T fact(int n) {
     static vector<T> fac = {1};
+    fac.reserve(n + 1);
     while(fac.size() <= n) {
         fac.push_back(fac.back() * int(fac.size()));
     }
     return fac[n];
 }
 template <typename T = mint> T fact_inv(int n) {
-    static vector<T> inv_fac = {1};
-    while(inv_fac.size() <= n) {
-        inv_fac.push_back(inv_fac.back() / int(inv_fac.size()));
+    static vector<T> inv_fac;
+    if(inv_fac.size() <= n) {
+        int old_size = inv_fac.size();
+        inv_fac.resize(n + 1);
+        inv_fac[n] = ~fact(n);
+        for(int i = n - 1; i >= old_size; i--) {
+            inv_fac[i] = inv_fac[i + 1] * (i + 1);
+        }
     }
     return inv_fac[n];
 }
