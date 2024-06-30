9d16aee60a5e17ede1246fd830f0294228d6c4e3
Vernon Gutierrez <verngutz@gmail.com>
2024-06-30 15:44:16 +0800

    BIT 0-index or 1-index options

--- Data Structs - BIT (1D).cpp
+++ Data Structs - BIT (1D).cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-// NOTE: one-indexed
-template <typename T> struct bit {
+template <typename T, bool Index> struct bit {
     const int n;
     vector<T> t;
     bit(int n) : n(n), t(n + 1) {}
     void add(int i, T value) {
-        for(int j = i; j <= n; j += j & -j) {
+        for(int j = i + 1 - Index; j <= n; j += j & -j) {
             t[j] += value;
         }
     }
     T sum(int i) {
         T res = 0;
-        for(int j = i; j > 0; j -= j & -j) {
+        for(int j = i + 1 - Index; j > 0; j -= j & -j) {
             res += t[j];
         }
         return res;
@@ -20,10 +19,13 @@ template <typename T> struct bit {
     T sum(int L, int R) {
         return sum(R) - sum(L - 1);
     }
+    void clear() {
+        fill(t.begin(), t.end(), 0);
+    }
     friend ostream& operator<<(ostream& os, bit& t) {
         os << "[";
-        for(int i = 1; i <= t.n; i++) {
-            if(i != 1) os << ", ";
+        for(int i = Index; i < t.n + Index; i++) {
+            if(i != Index) os << ", ";
             os << t.sum(i, i);
         }
         return os << "]";
