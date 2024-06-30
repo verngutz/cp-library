760eaf124cfcecd209f2e71d74fcc1a15000dc2c
Vernon Gutierrez <verngutz@gmail.com>
2024-06-30 15:43:10 +0800

    sparse table const factor optimization

--- Data Structs - Sparse Table.cpp
+++ Data Structs - Sparse Table.cpp
@@ -7,9 +7,10 @@ template <typename T> struct sparse_table {
     vector<vector<T>> st;
     sparse_table(const vector<T>& a, const monoid<T>& m)
     : size(a.size()), m(m), st(__lg(size) + 1, vector<T>(size, m.identity)) {
-        for(int pow2 = 0; pow2 <= __lg(size); pow2++) {
+        st[0] = a;
+        for(int pow2 = 1; pow2 <= __lg(size); pow2++) {
             for(int i = 0; i + (1 << pow2) - 1 < size; i++) {
-                st[pow2][i] = pow2 ? m.combine(st[pow2 - 1][i], st[pow2 - 1][i + (1 << (pow2 - 1))]) : a[i];
+                st[pow2][i] = m.combine(st[pow2 - 1][i], st[pow2 - 1][i + (1 << (pow2 - 1))]);
             }
         }
     }
