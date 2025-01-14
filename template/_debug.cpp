48f3820d4b1c05ce3e36be740019e87d7ae09aa6
Vernon Gutierrez <verngutz@gmail.com>
2025-01-14 22:04:18 +0800

    template upgrade to C++23

--- template/_debug.cpp
+++ template/_debug.cpp
@@ -1,47 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-ostream& print(ostream& os, auto L, auto R) {
+template <typename T1, typename T2> ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template <typename T> concept NonStringRange = ranges::range<T> and not is_convertible_v<T, string_view>; 
+template <NonStringRange T> ostream& operator<<(ostream& os, const T& c) {
     os << "[";
-    for(auto it = L; it != R; it++) {
-        if(it != L) os << ", ";
+    for(auto it = begin(c); it != end(c); it++) {
+        if(it != begin(c)) os << ", ";
         os << *it;
     }
     return os << "]";
-}
-template<typename T1, typename T2>
-ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
-    return os << "(" << p.first << ", " << p.second << ")";
-}
-template<typename T>
-struct is_container {
-private:
-    template<typename C> static char test_it(typename C::const_iterator*);
-    template<typename C> static long test_it(...);
-    template<typename C> static char test_st(typename enable_if<is_same<C, string>::value>::type*);
-    template<typename C> static long test_st(...);
-    template<typename C> static char test_be(typename enable_if<
-        is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::begin)),
-                typename C::const_iterator(C::*)() const>::value>::type*);
-    template<typename C> static long test_be(...);
-    template<typename C> static char test_en(typename enable_if<
-        is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::end)),
-                typename C::const_iterator(C::*)() const>::value>::type*);
-    template<typename C> static long test_en(...);
-public:
-    enum { value = sizeof(test_it<T>(0)) == sizeof(char)
-                && sizeof(test_st<T>(0)) == sizeof(long)
-                && sizeof(test_be<T>(0)) == sizeof(char)
-                && sizeof(test_en<T>(0)) == sizeof(char) };
-};
-template<typename T>
-typename enable_if<is_container<T>::value, ostream&>::type
-operator<<(ostream& os, const T& c) {
-    return print(os, c.begin(), c.end());
-}
-template<typename T>
-typename enable_if<!is_same<typename remove_cv<T>::type, char>::value, ostream&>::type
-operator<<(ostream& os, const T arr[]) {
-    return print(os, begin(arr), end(arr));
 }
 vector<string> split(const string& s, char c) {
     vector<string> v;
@@ -56,8 +25,7 @@ vector<string> split(const string& s, char c) {
 void error(vector<string>::iterator it) {
     cerr << "\033[0m" << endl;
 }
-template<typename T, typename... Args>
-void error(vector<string>::iterator it, T a, Args... args) {
+template <typename T, typename... Args> void error(vector<string>::iterator it, T a, Args... args) {
 	cerr << "\033[0;32;1m" << it->substr((*it)[0] == ' ', it->back() == ',' ? it->size() - 1 : it->size()) << " = " << a << "; ";
 	error(++it, args...);
 }
