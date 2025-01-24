c95ce8995bfe5457d009cbd540217b85fcca1215
Vernon Gutierrez <verngutz@gmail.com>
2025-01-25 02:16:05 +0800

    fix fast pow

--- Math - Fast Pow.cpp
+++ Math - Fast Pow.cpp
@@ -1,6 +1,6 @@
 #ifndef __FASTPOW_INCLUDED_
 #define __FASTPOW_INCLUDED_
-template <typename T> constexpr T fpow_eq(T& x, long long y, T identity = 1) {
+template <typename T> constexpr T& fpow_eq(T& x, long long y, T identity = 1) {
     for(; y > 0; x *= x, y >>= 1) if(y & 1) identity *= x;
     return x = identity;
 }
