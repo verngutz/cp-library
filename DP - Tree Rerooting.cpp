97a4af4121f61c19a666924671fc3f4151db8f9a
Vernon Gutierrez <verngutz@gmail.com>
2025-04-22 02:57:58 +0800

    minor aesthetic fix to tree rerooting

--- DP - Tree Rerooting.cpp
+++ DP - Tree Rerooting.cpp
@@ -5,7 +5,7 @@ template <typename T> concept GraftFunc = is_convertible_v<T, f<void(int, int)>>
 template <typename T> concept PruneFunc = is_convertible_v<T, f<void(int, int)>>;
 template <typename T> concept RootFunc = is_convertible_v<T, f<void(int)>>;
 template <bool Directed, typename TEdge, bool Index, GraftFunc Graft, PruneFunc Prune, RootFunc Root>
-void tree_rerooting(graph<Directed, TEdge, Index>& g, const Graft& graft, const Prune& prune, const Root& pos_node) {
+void tree_rerooting(graph<Directed, TEdge, Index>& g, const Graft& graft, const Prune& prune, const Root& root) {
     dfs(g, [&](dfs_params<TEdge>& p) {
         p.pos_edge = [&](TEdge& e) {
             graft(e.u, e.v);
@@ -16,7 +16,7 @@ void tree_rerooting(graph<Directed, TEdge, Index>& g, const Graft& graft, const 
             prune(e.u, e.v);
             graft(e.v, e.u);
         };
-        p.pos_node = pos_node;
+        p.pos_node = root;
         p.pos_edge = [&](TEdge& e) {
             prune(e.v, e.u);
             graft(e.u, e.v);
